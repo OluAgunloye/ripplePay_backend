7c5647cb23c9e70dddbb5590dfb04166c5a313e1
pateldd1 <devanshpatel22@gmail.com>
2018-02-14 00:53:50 -0800

    change  global variable redis instance to local

--- scripts/test.js
+++ scripts/test.js
@@ -1,32 +1,67 @@
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
 const Promise = require('bluebird');
+const client = require('redis').createClient();
+const { promisify } = require('util');
+const lock = promisify(require('redis-lock')(client));
 
-const fn = async(function(){
-    let y = 1000;
-    while (y < 2000) {
-        y += 1
-        console.log(y);
-    }
-    return false;
-});
-
-const bn = function(){
-    let y = 3000;
-    while (y < 4000) {
-        y += 1
-        console.log(y);
-    }
-    return Promise.resolve(true);
-};
-
-bn().then((val) => {
-    console.log(val);
+let practice1 = async(function() {
+    let unlock = await(lock("my-lock"));
+    setTimeout(() => {
+        try {
+            console.log("hellllow");  
+            throw 'error';
+            unlock();
+        } catch(e) {
+            console.log(e);  
+        } finally {
+            unlock();
+        }
+    }, 1);
 })
 
-console.log(fn());
-let y = 1;
-while (y < 1000) {
-    y += 1
-    console.log(y);
+let practice2 = async(function() {
+    let unlock = await(lock("my-lock"));
+    console.log("yoyoyoyo");
+    unlock();
+})
+
+practice1();
+practice2();
+
+if ("hello") {
+    let x = 1;
+} else {
+    let x = 3;
 }
+console.log(x);
+
+
+// const fn = async(function(){
+//     let y = 1000;
+//     while (y < 2000) {
+//         y += 1
+//         console.log(y);
+//     }
+//     return false;
+// });
+
+// const bn = function(){
+//     let y = 3000;
+//     while (y < 4000) {
+//         y += 1
+//         console.log(y);
+//     }
+//     return Promise.resolve(true);
+// };
+
+// bn().then((val) => {
+//     console.log(val);
+// })
+
+// console.log(fn());
+// let y = 1;
+// while (y < 1000) {
+//     y += 1
+//     console.log(y);
+// }
